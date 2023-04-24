a8a912188ae648b934e30055e01cae5de4cecbd5
Jyoti <jyoti.duwal@outcodesoftware.com>
2023-04-24 15:16:52 +0545

    Upgraded the version of django to 4.2.

--- app/hello_django/views.py
+++ app/hello_django/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
+import django
 
 def student_show(request):
     cursor = connection.cursor()
@@ -10,4 +11,8 @@ def student_show(request):
     x = []
     for i in range(10):
         x.append(i)
-    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
+
+    django_version = django.VERSION
+    print(django_version)
+    test_response = "<h1>DataFlair Django </h1>The Digits are {0}".format(x) + "django version =>" + ''
+    return HttpResponse(test_response)
